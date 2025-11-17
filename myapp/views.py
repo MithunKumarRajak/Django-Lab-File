1b3e280b70df6370fbbe3e744fa5107080ade732
MithunKumarRajak <mithunkumarrajak01012005@gmail.com>
2025-11-17 21:31:16 +0530

    Added Notes

--- myapp/views.py
+++ myapp/views.py
@@ -7,15 +7,18 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'myapp/home.html')
 
+
 def about(request):
     return render(request, 'myapp/about.html')
+
 
 def menu(request):
     return render(request, 'myapp/menu.html')
 
+
 def booking(request):
     return render(request, 'myapp/booking.html')
 
+
 def contact(request):
     return render(request, 'myapp/contact.html')
-
--- note/Convension.md
+++ note/Convension.md
@@ -1,1 +1,18 @@
-## Use PascalCase for model names   - e.g. Recipe
+# Use PascalCase for model names   - e.g. Recipe
+
+## 1. app ka naam small letter se start Karna
+
+shop
+users
+analytics
+chat
+
+## 2. Model ka naam capital letter se start Kare
+
+Student
+Employee
+
+## 3. views ka naam plurals mai likhe
+
+students
+employees
